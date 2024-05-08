7ad7c92b4e09f30a70b51b844ba747d209363d3f
Zack <zack.casparro@pm.me>
2024-05-08 12:39:50 -0400

    Complete TOP EAS project

--- script.js
+++ script.js
@@ -4,27 +4,33 @@ function createSquares (number = 16) {
         const contentContainer = document.querySelector("#container");
         content.classList.add("square");
         contentContainer.appendChild(content);
+
+        let squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("mouseover", function (e) {
+                e.target.style.background = "red";
+            });
+        });
     };
 };
 
 window.onload = createSquares();
 
 
-
+let squares = document.querySelectorAll(".square");
 const button = document.querySelector(".input-squares");
 button.addEventListener("click", () => {
     number = prompt("how many squares?");
+
+    if (number > 20) {
+        number = prompt("Please enter a number less than 70.")
+    };
     squares.forEach((square) => {
         square.remove();
     });
     createSquares(number);
 });
 
-const squares = document.querySelectorAll(".square");
 
 
-squares.forEach((square) => {
-    square.addEventListener("mouseover", function (e) {
-        e.target.style.background = "red";
-    });
-});
+
